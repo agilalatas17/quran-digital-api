fd388c0a6e903c47b4e02bd6008c3f28895c8688
Agil_Alatas <agilalatas17@gmail.com>
2022-05-27 01:56:43 +0700

    mengubah konfigurasi source konten

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-    mode: "jit",
-    purge: {
-        enabled: true,
-        content: ["./src/**/*.{html,ts}"],
-    },
+    content: ["./src/**/*.{html,ts}"],
+
     theme: {
         fontSize: {
             // sm: ".75rem",
